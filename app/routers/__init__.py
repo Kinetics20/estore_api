2fdea9d364603f59fd8c374b531504316077c8d9
Kinetics20 <kinetics20@gmail.com>
2025-05-22 22:46:42 +0200

    completed products router and created pydantic validator for it

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,5 +1,7 @@
 from app.routers.products import router as products_router
+from app.routers.locations import router as locations_router
 
 routers = [
     products_router,
+    locations_router,
 ]
